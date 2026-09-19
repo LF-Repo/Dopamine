1d0d16d2ebdba13e6a64e45743cd5619666dbbd6
LF <lf-repo334@outlook.com>
2026-09-19 11:03:18 +0800

    DOSettingsController.m: try fix

--- Application/Dopamine/UI/Settings/DOSettingsController.m
+++ Application/Dopamine/UI/Settings/DOSettingsController.m
@@ -20,10 +20,15 @@
 #import "DOSceneDelegate.h"
 #import "DOPSJetsamListItemsController.h"
 #import "DOButtonCell.h"
-#import "DOAppHidePickerViewController.h"
 
 @interface DOSettingsController ()
 
+@end
+
+@interface DOAppHidePickerViewController : UITableViewController <UISearchResultsUpdating>
+@property (nonatomic, strong) NSMutableArray<NSMutableDictionary *> *allApps;
+@property (nonatomic, strong) NSMutableArray<NSMutableDictionary *> *filteredApps;
+@property (nonatomic, strong) UISearchController *searchController;
 @end
 
 @implementation DOSettingsController
@@ -797,6 +802,117 @@
 {
     DOAppHidePickerViewController *picker = [[DOAppHidePickerViewController alloc] initWithStyle:UITableViewStyleInsetGrouped];
     [self.navigationController pushViewController:picker animated:YES];
+}
+
+@end
+
+@implementation DOAppHidePickerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"按应用隐藏";
+    self.tableView.rowHeight = 56;
+
+    self.allApps = [NSMutableArray array];
+    self.filteredApps = [NSMutableArray array];
+
+    [self loadInstalledApps];
+
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    self.searchController.searchResultsUpdater = self;
+    self.searchController.obscuresBackgroundDuringPresentation = NO;
+    self.navigationItem.searchController = self.searchController;
+    self.navigationItem.hidesSearchBarWhenScrolling = NO;
+    self.definesPresentationContext = YES;
+}
+
+- (void)loadInstalledApps
+{
+    Class LSApplicationWorkspace_class = objc_getClass("LSApplicationWorkspace");
+    if (!LSApplicationWorkspace_class) return;
+
+    NSObject *workspace = [LSApplicationWorkspace_class performSelector:NSSelectorFromString(@"defaultWorkspace")];
+    NSArray *allApps = [workspace performSelector:NSSelectorFromString(@"allApplications")];
+    DOEnvironmentManager *env = [DOEnvironmentManager sharedManager];
+
+    for (id app in allApps) {
+        NSString *bundleID = [app valueForKey:@"applicationIdentifier"];
+        NSString *name = [app valueForKey:@"localizedName"];
+        if (!bundleID || !name) continue;
+
+        BOOL hidden = [env isEnvironmentHiddenForBundleID:bundleID];
+        [self.allApps addObject:[@{
+            @"bundleID": bundleID,
+            @"name": name,
+            @"hidden": @(hidden)
+        } mutableCopy]];
+    }
+
+    [self.allApps sortUsingComparator:^NSComparisonResult(NSDictionary *a, NSDictionary *b) {
+        return [a[@"name"] localizedCaseInsensitiveCompare:b[@"name"]];
+    }];
+
+    [self.filteredApps setArray:self.allApps];
+    [self.tableView reloadData];
+}
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
+{
+    NSString *text = searchController.searchBar.text ?: @"";
+    if (text.length == 0) {
+        [self.filteredApps setArray:self.allApps];
+    } else {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name CONTAINS[cd] %@ OR bundleID CONTAINS[cd] %@", text, text];
+        NSArray *filtered = [self.allApps filteredArrayUsingPredicate:predicate];
+        [self.filteredApps setArray:filtered];
+    }
+    [self.tableView reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.filteredApps.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellId = @"AppHideCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+
+    NSDictionary *appInfo = self.filteredApps[indexPath.row];
+    cell.textLabel.text = appInfo[@"name"];
+    cell.detailTextLabel.text = appInfo[@"bundleID"];
+    cell.detailTextLabel.textColor = [UIColor secondaryLabelColor];
+    cell.detailTextLabel.font = [UIFont monospacedSystemFontOfSize:11 weight:UIFontWeightRegular];
+
+    UISwitch *toggle = [[UISwitch alloc] init];
+    toggle.on = [appInfo[@"hidden"] boolValue];
+    toggle.tag = indexPath.row;
+    [toggle addTarget:self action:@selector(toggleChanged:) forControlEvents:UIControlEventValueChanged];
+    cell.accessoryView = toggle;
+
+    return cell;
+}
+
+- (void)toggleChanged:(UISwitch *)sender
+{
+    NSDictionary *appInfo = self.filteredApps[sender.tag];
+    NSString *bundleID = appInfo[@"bundleID"];
+    BOOL hidden = sender.on;
+
+    [[DOEnvironmentManager sharedManager] setEnvironmentHidden:hidden forBundleID:bundleID];
+
+    for (NSMutableDictionary *dict in self.allApps) {
+        if ([dict[@"bundleID"] isEqualToString:bundleID]) {
+            dict[@"hidden"] = @(hidden);
+            break;
+        }
+    }
 }
 
 @end
