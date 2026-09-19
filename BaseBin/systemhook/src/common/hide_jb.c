bc4e01379ba9188c17c9a8abff7991bce132d10e
LF <lf-repo334@outlook.com>
2026-09-19 14:27:21 +0800

    hide_jb.c: lite hook

--- BaseBin/systemhook/src/common/hide_jb.c
+++ BaseBin/systemhook/src/common/hide_jb.c
@@ -197,12 +197,5 @@ void hide_jb_enable(void)
 
     litehook_hook_function(open,       open_hook);
     litehook_hook_function(openat,     openat_hook);
-    litehook_hook_function(stat,       stat_hook);
-    litehook_hook_function(lstat,      lstat_hook);
-    litehook_hook_function(fstatat,    fstatat_hook);
-    litehook_hook_function(access,     access_hook);
-    litehook_hook_function(statfs,     statfs_hook);
-    litehook_hook_function(readlink,   readlink_hook);
-    litehook_hook_function(readlinkat, readlinkat_hook);
     litehook_hook_function(opendir,    opendir_hook);
 }
