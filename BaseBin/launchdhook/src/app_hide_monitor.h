5374ba7ea8ee421ba64ca16a771d0f285c00bffc
LF <lf-repo334@outlook.com>
2026-09-25 07:42:48 +0800

    update app_hide_monitor.h

--- BaseBin/launchdhook/src/app_hide_monitor.h
+++ BaseBin/launchdhook/src/app_hide_monitor.h
@@ -1,6 +1,10 @@
 #ifndef APP_HIDE_MONITOR_H
 #define APP_HIDE_MONITOR_H
 
+#include <stdbool.h>
+
 void start_app_hide_monitor(void);
+bool app_hide_is_target(const char *executablePath);
+void app_hide_perform_hide_sync(void);
 
 #endif
